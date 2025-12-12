13f1905f8cd521d15bfbeb68204139944af643cc
KawumaLatif <kawumalatiif5@gmail.com>
2025-12-12 16:16:02 +0300

    initial commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex">
       <h1 className="text-white text-4xl">Hello</h1>
     </div>
   );
